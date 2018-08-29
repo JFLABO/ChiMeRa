1fec230fe568bafdbf99f9e4c19a153cf3756fc8
Tokitaka Abe <vortex003@msn.com>
2018-08-29 18:27:23 +0900

    api fix

--- api/json.php
+++ api/json.php
@@ -137,7 +137,13 @@ for ($i = 0; $i < count($stack); $i++) {
       //YYYY/MM/DDの日付形式を抽出する
       preg_match('|\d{4}\/\d{1,2}\/\d{1,2}|', $date_text, $date_match);
       echo "\"".$date_match[0]."\""."\n";
-      echo "},";
+
+      echo "}";
+      $a=count($stack)-1;
+      if($a!=$i){
+            echo ",";
+      }
+
 }
 echo "]";
 
