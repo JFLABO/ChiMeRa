10791a83d37bc1e3b2f2198ed60378d73c2f8e23
JFLABO[JP] <vortex003@msn.com>
2019-07-30 19:25:49 +0900

    Create .init.el

---
+++ .init.el
@@ -0,0 +1,18 @@
+;emacsでF5押したら「2010/09/22 18:29:20」のような日付が挿入される設定。
+(define-key global-map [f5]
+  '(lambda ()
+     (interactive)
+          (insert (format-time-string "%Y/%m/%d %H:%M:%S"))))
+          
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
+;パッケージ情報を最新に更新
+;M-x package-refresh-contents
+;M-x package-list-packages
+
+;fuzzy
+;php-mode
+;auto-complete
